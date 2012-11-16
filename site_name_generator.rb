d8b61c8ac7aaf195456b3a75cba4d4936fcc064f
Nicholas Greenfield <Greenie0506@gmail.com>
2012-11-16 14:34:07 -0500

    Update site_name_generator.rb
    refactor domain name combinations

--- site_name_generator.rb
+++ site_name_generator.rb
@@ -2,24 +2,17 @@ require 'whois'
 
 words = ["date", "dash", "breaker", "runaway", "escape", "bail", "phone", "call", "relationship", "blinddate", "dinner", "peaceout"]
 
-r = words.map do |word|
-  words.reject{ |item| item == word }.map do |w|
-    combo = word + w
-    combo + ".com"
-  end
+results = words.permutation(2).map do |w1,w2|
+  domain = "#{w1}#{w2}.com"
 end
-
-result = r.flatten
-
 
 c = Whois::Client.new
 
-a = result.reject do|item|
+a = results.reject do |item|
   r = c.query("#{item}")
   print "."
   r.available? == false
 end
 
 puts a.inspect
-
 
